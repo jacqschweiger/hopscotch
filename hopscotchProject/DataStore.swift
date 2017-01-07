b5bf2f84e48432ece27d3c7aed1641cecd1f07c7
jacqschweiger <jacqschweiger@gmail.com>
2017-01-06 21:13:01 -0500

    loads correct data, moved bracket in datastore

--- hopscotchProject/DataStore.swift
+++ hopscotchProject/DataStore.swift
@@ -25,9 +25,10 @@ class DataStore {
             for project in JSON {
                 let newProject = Project(dictionary: project)
                 self.projects.append(newProject)
+                print(newProject.title)
             }
-        }
         completion()
+        }
     }
     
     
