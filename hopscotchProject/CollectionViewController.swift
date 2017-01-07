26ec28f19d9b76b515ccfbcc42071e09b81cc4b4
jacqschweiger <jacqschweiger@gmail.com>
2017-01-07 10:34:30 -0500

    added basic layout framework for collection view

--- hopscotchProject/CollectionViewController.swift
+++ hopscotchProject/CollectionViewController.swift
@@ -9,9 +9,22 @@
 import Foundation
 import UIKit
 
-class CollectionViewController: UICollectionViewController {
+class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     var store = DataStore.sharedInstance
+    
+    var sectionInsets: UIEdgeInsets!
+    var spacing: CGFloat! = 2
+    var itemSize: CGSize!
+    var referenceSize: CGSize! = CGSize(width: 60, height: 60)
+    var numberOfRows: CGFloat! = 4
+    var numberOfColumns: CGFloat! = 3
+    var screenHeight = UIScreen.main.bounds.height
+    var screenWidth = UIScreen.main.bounds.width
+    
+    var itemWidth: CGFloat!
+    var itemHeight: CGFloat!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +34,8 @@ class CollectionViewController: UICollectionViewController {
                self.collectionView?.reloadData()
             }
         }
+        
+        configureLayout()
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -38,6 +53,44 @@ class CollectionViewController: UICollectionViewController {
         
         return cell
 
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return spacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return spacing
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return itemSize
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return referenceSize
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return referenceSize
+    }
+    
+    func configureLayout(){
+        sectionInsets = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        
+        itemWidth = (screenWidth/numberOfColumns) - ((spacing * 4) / 3)
+        itemHeight = (screenHeight/numberOfRows) - ((spacing * 5) / 4)
+        itemSize = CGSize(width: itemWidth, height: itemHeight)
+        
+        
     }
 
 
