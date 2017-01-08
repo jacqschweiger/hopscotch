140d6b99def0ec8d9d6bff1abf3f78024b609e99
jacqschweiger <jacqschweiger@gmail.com>
2017-01-08 17:04:46 -0500

    removed print statements, added testing bundle

--- hopscotchProject/DataStore.swift
+++ hopscotchProject/DataStore.swift
@@ -24,14 +24,10 @@ class DataStore {
             for project in JSON {
                 let newProject = Project(dictionary: project)
                 self.projects.append(newProject)
-                print(newProject.title)
             }
             
         completion()
             
         }
     }
-    
-    
-    
 }
